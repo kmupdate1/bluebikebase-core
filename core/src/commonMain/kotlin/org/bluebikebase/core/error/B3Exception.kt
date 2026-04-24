25da6eadef222b0d935e9ebfc891f85c23195cc9
Ken <lax.potcx1562@gmail.com>
2026-04-24 12:18:22 +0900

    可視性バグ修正

--- core/src/commonMain/kotlin/org/bluebikebase/core/error/B3Exception.kt
+++ core/src/commonMain/kotlin/org/bluebikebase/core/error/B3Exception.kt
@@ -1,6 +1,6 @@
 package org.bluebikebase.core.error
 
-class B3Exception(
+open class B3Exception(
     override val message: String,
     override val cause: Throwable? = null
 ) : CoreException(message = message, cause = cause)
