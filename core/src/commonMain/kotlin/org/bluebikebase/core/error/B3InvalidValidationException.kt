122dbb17f9ec9fba5cc9c1432ce4b98b1478055e
Ken <lax.potcx1562@gmail.com>
2026-04-24 15:47:57 +0900

    例外メッセージnull許容に変更

--- core/src/commonMain/kotlin/org/bluebikebase/core/error/B3InvalidValidationException.kt
+++ core/src/commonMain/kotlin/org/bluebikebase/core/error/B3InvalidValidationException.kt
@@ -1,6 +1,6 @@
 package org.bluebikebase.core.error
 
 open class B3InvalidValidationException(
-    override val message: String,
+    override val message: String?,
     override val cause: Throwable? = null,
 ) : B3Exception(message = message, cause = cause)
