309fb186504dc28f79ad17f6c2af5040276245fa
Ken <lax.potcx1562@gmail.com>
2026-04-29 13:10:37 +0900

    B3Hash.ktにequals()追加

--- core/src/commonMain/kotlin/org/bluebikebase/core/identity/B3Hash.kt
+++ core/src/commonMain/kotlin/org/bluebikebase/core/identity/B3Hash.kt
@@ -1,10 +1,9 @@
 package org.bluebikebase.core.identity
 
 import org.bluebikebase.core.quantity.HeartBeat
-import kotlin.jvm.JvmInline
 
-@JvmInline
-value class B3Hash private constructor(val id: ByteArray) : UniqueID {
+@ConsistentCopyVisibility
+data class B3Hash private constructor(val id: ByteArray) : UniqueID {
     companion object {
         fun fromBytes(bytes: ByteArray): B3Hash = B3Hash(generateHash(bytes))
 
@@ -13,4 +12,12 @@ value class B3Hash private constructor(val id: ByteArray) : UniqueID {
             return B3Hash(generateHash(now.toString().encodeToByteArray()))
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is B3Hash) return false
+        return this.id.contentEquals(other.id)
+    }
+
+    override fun hashCode(): Int = id.contentHashCode()
+    override fun toString(): String = id.contentToString()
 }
