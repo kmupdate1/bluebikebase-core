c192486e3d1cab7622c933856a50f8a6939560c0
Ken <lax.potcx1562@gmail.com>
2026-04-29 09:49:29 +0900

    必然的な（意図的な）ID生成強化

--- core/src/commonMain/kotlin/org/bluebikebase/core/identity/B3Hash.kt
+++ core/src/commonMain/kotlin/org/bluebikebase/core/identity/B3Hash.kt
@@ -6,6 +6,8 @@ import kotlin.jvm.JvmInline
 @JvmInline
 value class B3Hash private constructor(val id: ByteArray) : UniqueID {
     companion object {
+        fun fromBytes(bytes: ByteArray): B3Hash = B3Hash(generateHash(bytes))
+
         fun gen(): UniqueID {
             val now = HeartBeat.now()
             return B3Hash(generateHash(now.toString().encodeToByteArray()))
